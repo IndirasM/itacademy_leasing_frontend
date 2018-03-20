9288bb70fea27f072f516bdbbd5b6e86a19a65db
sostakas <ernestas.sostakas@gmail.com>
2018-03-20 15:53:14 +0200

    added firstName and Comment field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {LeasingFormComponent} from './leasing-form/leasing-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeasingFormComponent
   ],
   imports: [
     BrowserModule,
--- src/app/leasing-form/leasing-form.component.ts
+++ src/app/leasing-form/leasing-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-leasing-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeasingFormComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
