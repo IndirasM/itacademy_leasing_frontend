147f2d61cf3bc82ea12d0f7bd15a75cb7e8fe38f
aurkut96 <33968892+aurkut96@users.noreply.github.com>
2018-03-23 11:34:48 +0200

    ASDAD
    ASDAD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ContractFormComponent } from './contract-form/contract-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContractFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/private/private_summary_show/private-leasing-data-form.component.ts
@@ -0,0 +1,137 @@
+import { Component, OnInit } from '@angular/core';
+import { PrivateLeasingFormDialogComponent } from './private-leasing-form-dialog-component';
+import {MAT_DIALOG_DATA, MatDialogRef, MatDialogConfig, MatDialog} from '@angular/material';
+
+import { Validators, FormGroup, FormBuilder, FormControl } from '@angular/forms';
+// import {ControlGroup} from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+
+@Component({
+  selector: 'apzp-private-leasing-data-form',
+  styles: [`.form-field { padding: 10px 0px; }`],
+  templateUrl: './private-leasing-data-form.component.html',
+  styleUrls: ['./private-leasing-data-form.component.css']
+})
+export class PrivateLeasingDataFormComponent implements OnInit {
+  dialog: any;
+  productTypes = ['Alfa Romeo',
+   'Audi', 'BMW', 'Ford', 'Honda', 'Jaguar', 'Lamborghini',
+    'Lexus', 'Mazda', 'Mercedes-Benz', 'Nissan', 'Peugeot',
+    'Subaru', 'Volkswagen'];
+  assetTypes = ['Vehicle'];
+  years = ['1980', '1981'];
+  allProducts = [
+    {name: '147', type: 'Alfa Romeo'},
+    {name: '155', type: 'Alfa Romeo'},
+    {name: 'Giulia', type: 'Alfa Romeo'},
+    {name: 'A2', type: 'Audi'},
+    {name: 'A3', type: 'Audi'},
+    {name: 'A4', type: 'Audi'},
+    {name: 'R8', type: 'Audi'},
+    {name: 'TT', type: 'Audi'},
+    {name: '3', type: 'BMW'},
+    {name: '5', type: 'BMW'},
+    {name: '6', type: 'BMW'},
+    {name: 'X3', type: 'BMW'},
+    {name: 'X5', type: 'BMW'},
+    {name: 'Focus', type: 'Ford'},
+    {name: 'Mustang', type: 'Ford'},
+    {name: 'S-Type', type: 'Jaguar'},
+    {name: 'XFR', type: 'Jaguar'},
+    {name: 'XJ13', type: 'Jaguar'},
+    {name: 'Countach', type: 'Lamborghini'},
+    {name: 'Gallardo', type: 'Lamborghini'},
+    {name: 'GS', type: 'Lexus'},
+    {name: 'IS', type: 'Lexus'},
+    {name: 'RX-7', type: 'Mazda'},
+    {name: 'Mazda6', type: 'Mazda'},
+    {name: 'MX-5', type: 'Mazda'},
+    {name: 'A', type: 'Mercedes-Benz'},
+    {name: 'C', type: 'Mercedes-Benz'},
+    {name: 'CL', type: 'Mercedes-Benz'},
+    {name: 'CLK', type: 'Mercedes-Benz'},
+    {name: 'Primera', type: 'Nissan'},
+    {name: 'Silvia', type: 'Nissan'},
+    {name: 'Skyline', type: 'Nissan'},
+    {name: '307', type: 'Peugeot'},
+    {name: '908', type: 'Peugeot'},
+    {name: 'Impreza', type: 'Subaru'},
+    {name: 'Legacy', type: 'Subaru'},
+    {name: 'Golf', type: 'Wolkswagen'},
+    {name: 'Polo', type: 'Wolkswagen'},
+    {name: 'Accord', type: 'Honda'},
+    {name: 'Civic', type: 'Honda'},
+  ];
+  productsAfterChangeEvent = [];
+  productForm: FormGroup;
+
+
+  constructor(fb: FormBuilder) {
+    this.productForm = fb.group({
+      productType: [],
+      productModel: [],
+      assetType: [],
+      customerType: new FormControl('Business', Validators.required),
+      yearMake: new FormControl('gay') ,
+      enginePower: new FormControl('', Validators.required),
+      assetPrice: new FormControl('', Validators.required),
+      advancePaymentPercentage: new FormControl('', Validators.required),
+      advancePaymentAmount: new FormControl('', Validators.required),
+      leasePeriod: new FormControl('', Validators.required),
+      marginM: new FormControl('', Validators.required),
+      contactFee: new FormControl('', Validators.required),
+      paymentDate: new FormControl('', Validators.required)
+
+    });
+  }
+   // Rebuild the product list every time the product type changes.
+  typeChanged() {
+    const productType = this.productForm.get('productType').value;
+    this.productsAfterChangeEvent = this.allProducts.filter(p => p.type === productType);
+
+    const assetType = this.productForm.get('assetType').value;
+    this.productsAfterChangeEvent = this.allProducts.filter(p => p.type === assetType);
+  }
+
+  submitForm() {
+    console.log('Form Data', this.productForm.value);
+  }
+  ngOnInit() {
+
+  }
+  openDialog() {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.data = {
+      id: 1,
+      title: 'Angular For Beginners'
+  };
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    this.dialog.open(PrivateLeasingFormDialogComponent, dialogConfig);
+  }
+
+  penDialog() {
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+
+    dialogConfig.autoFocus = true;
+
+    dialogConfig.data = {
+        id: 1,
+        title: 'Angular For Beginners'
+    };
+
+    this.dialog.open(PrivateLeasingFormDialogComponent, dialogConfig);
+
+    const dialogRef = this.dialog.open(PrivateLeasingFormDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(
+        data => console.log('Dialog output:', data)
+    );
+}
+}
