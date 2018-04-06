9c328f7b31869bf9e2593493e97f01700cc26fde
aurkut96 <33968892+aurkut96@users.noreply.github.com>
2018-04-06 16:31:51 +0300

    dwad

--- src/app/submitted-form-search/submitted-form-search.component.ts
+++ src/app/submitted-form-search/submitted-form-search.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LeaseToUserService} from '../services/leasing-to-user.service';
+import {FormsToBackService} from '../services/forms-to-back.service';
+import {LeaseData} from '../private/private-leasing-data-form/private-leasing-data';
+import {PrivateUserData} from '../private/private-user-data-form/privateUserData';
+import {CorporateUserData} from '../corporate/corporate-user-data-form/corporateUserData';
 
 @Component({
   selector: 'app-submitted-form-search',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmittedFormSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private leaseService: LeaseToUserService,
+    ) {}
+
+  public leaseData: LeaseData;
+  public userData: PrivateUserData;
+  private corporateUserData: CorporateUserData;
 
   ngOnInit() {
+    this.leaseService.toSend.subscribe(leaseData => {
+      if (leaseData) {
+        this.leaseData = leaseData;
+      }
+    });
+    this.leaseService.toSendUser.subscribe(userData => {
+      if (userData) {
+        this.userData = userData;
+      }
+    });
+    this.leaseService.toSendCorporate.subscribe(corporateUserData => {
+      this.corporateUserData = corporateUserData;
+    });
   }
 
 }
