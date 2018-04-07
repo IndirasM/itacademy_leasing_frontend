73c6ab431b88239dc222ae896ffc63662b9e83d5
Tolvinas <tolvinasvysniunas@gmail.com>
2018-04-07 13:29:46 +0300

    Filling the private form, but with sleeps :(

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,61 @@
-import { AppPage } from './app.po';
-
+import {AppPage} from './app.po';
+import {browser} from 'protractor';
+import { ElementFinder, promise } from 'protractor';
 describe('carleasingfront App', () => {
   let page: AppPage;
 
   beforeEach(() => {
+    browser.driver.manage().window().setSize(1920, 1080);
     page = new AppPage();
+    page.navigateTo();
+
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+
+  it('it should fill the private form', () => {
+    page.clickPrivate();
+
+    page.clickAssetType();
+    page.selectAssetType('Vehicle');
+
+    page.clickBrand();
+    page.selectBrand('Volkswagen');
+
+    page.clickModel();
+    page.selectModel('Tiguan');
+
+    page.clickYear();
+    page.selectCarYear('2005');
+
+
+    page.insertEnginePower(420);
+    page.insertAssetPrice(5406);
+
+    page.changeAdvancedPaymentPercentage();
+    page.changeLeasePeriod();
+    page.changeMargin();
+    page.clickPaymentDate(15);
+
+
+    //page.printSliderDimensions();
+    //browser.sleep(1000);
+    page.clickNextInCarLeasingForm();
+
+    //browser.sleep(500);
+    page.insertName('Dovydas');
+    page.insertSurname('Pavel');
+    page.insertId('39706270117');
+    page.insertPhoneNumber('867326074');
+    page.insertEmail('test@hehe.xd');
+    page.insertAddress('Topo centro 10');
+    page.clickNextInUserForm();
+    browser.sleep(500);
+    page.clickSubmit();
+    browser.sleep(500);
+    page.clickFinish();
+    browser.sleep(500);
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:49152/home');
   });
+
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,122 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element, ElementFinder, promise, protractor} from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  clickPrivate() {
+    this.waitingToBeClickableAndClicking(element(by.css('[id="privateButton"]')));
+  }
+
+  clickAssetType() {
+    this.waitingToBeClickableAndClicking(element(by.css('[placeholder="Asset Type"]')));
+  }
+
+  selectAssetType(assetType: string) {
+    this.waitingToBeClickableAndClicking(element(by.cssContainingText('.mat-select-content.ng-trigger.ng-trigger-fadeInContent span', assetType)));
+  }
+
+  clickBrand() {
+    this.waitingToBeClickableAndClicking(element(by.css('[placeholder="Brand"]')));
+  }
+
+  selectBrand(brand) {
+    this.waitingToBeClickableAndClicking(element(by.cssContainingText('.mat-select-content.ng-trigger.ng-trigger-fadeInContent span', brand)));
+  }
+
+  clickModel() {
+    this.waitingToBeClickableAndClicking(element(by.css('[placeholder="Model"]')));
+  }
+
+  selectModel(model: string) {
+    this.waitingToBeClickableAndClicking(element(by.cssContainingText('.mat-select-content.ng-trigger.ng-trigger-fadeInContent span', model)));
+  }
+
+  clickYear() {
+    this.waitingToBeClickableAndClicking(element(by.css('mat-select[formcontrolname="year"]')));
+  }
+
+  selectCarYear(year: string) {
+    this.waitingToBeClickableAndClicking(element(by.cssContainingText('.mat-select-content.ng-trigger.ng-trigger-fadeInContent span', year)));
+  }
+
+  insertEnginePower(num: number) {
+    element(by.css('[ng-reflect-name="enginePower"]')).sendKeys(num);
+  }
+
+  insertAssetPrice(num: number) {
+    element(by.css('[ng-reflect-name="assetPrice"]')).sendKeys(num);
+  }
+
+  changeAdvancedPaymentPercentage() {
+    element(by.css('[ng-reflect-name="advancePaymentPercentage"]')).click();
+  }
+
+  changeLeasePeriod() {
+    element(by.css('[ng-reflect-name="leasePeriod"]')).click();
+  }
+
+  changeMargin() {
+    element(by.css('[ng-reflect-name="margin"]')).click();
+  }
+
+  clickPaymentDate(num: number) {
+    this.waitingToBeClickableAndClicking(element(by.cssContainingText('[ng-reflect-name="paymentDate"] mat-radio-button', num.toString())));
+  }
+
+  clickNextInCarLeasingForm() {
+    this.waitingToBeClickableAndClicking(element(by.css('[id="nextButtonLeasingForm"]')));
+  }
+
+  insertName(name: string) {
+    browser.wait(protractor.ExpectedConditions.elementToBeClickable(element(by.css('[ng-reflect-name="firstName"]'))));
+    element(by.css('[ng-reflect-name="firstName"]')).sendKeys(name);
+  }
+
+  insertSurname(surname: string) {
+    element(by.css('[ng-reflect-name="lastName"]')).sendKeys(surname);
+  }
+
+  insertId(id: string) {
+    element(by.css('[ng-reflect-name="personalCode"]')).sendKeys(id);
+  }
+
+  insertPhoneNumber(phoneNumber: string) {
+    element(by.css('[ng-reflect-name="phoneNumber"]')).sendKeys(phoneNumber);
+  }
+
+  insertEmail(email: string) {
+    element(by.css('[ng-reflect-name="email"]')).sendKeys(email);
+  }
+
+  insertAddress(address: string) {
+    element(by.css('[ng-reflect-name="address"]')).sendKeys(address);
+  }
+
+  printSliderDimensions() {
+    const we = element(by.css('[ng-reflect-name="advancePaymentPercentage"]')).getWebElement();
+    browser.executeScript('return arguments[0].getClientRects()', we).then(re => {
+      console.log('client rects: ', re);
+    });
+  }
+
+  clickSubmit() {
+    this.waitingToBeClickableAndClicking(element(by.css('[id="previewSubmitButton"]')));
+  }
+
+  clickFinish() {
+    this.waitingToBeClickableAndClicking(element(by.css('[id="finishButton"]')));
+  }
+
+  clickNextInUserForm() {
+    this.waitingToBeClickableAndClicking(element(by.css('[id="nextButtonUserForm"]')));
+  }
+
+
+  private waitingToBeClickableAndClicking(value) {
+    browser.wait(protractor.ExpectedConditions.elementToBeClickable(value), 500);
+    value.click();
   }
 }
+
