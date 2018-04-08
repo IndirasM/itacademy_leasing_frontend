ef5bdeff03940d8b5495ba79e80995bc126c61bf
sostakas <ernestas.sostakas@gmail.com>
2018-04-08 22:29:34 +0300

    removed unnecessary validator

--- src/app/private/private-leasing-data-form/private-leasing-data-form.component.ts
+++ src/app/private/private-leasing-data-form/private-leasing-data-form.component.ts
@@ -78,7 +78,7 @@ export class PrivateLeasingDataFormComponent implements OnInit {
       brand: new FormControl([], Validators.required),
       model: new FormControl([], Validators.required),
       assetType: new FormControl([], Validators.required),
-      customerType: new FormControl(null, Validators.required),
+      customerType: new FormControl(null),
       year: new FormControl([], Validators.required),
       assetPrice: this.assetPriceValidator(
         (this.userType === 'Private') ? 5000 : 10000
